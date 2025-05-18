f2f88b6155ce0f836740a153bb4875b178c0cd72
Deleon <delps.santos1987@gmail.com>
2025-05-18 19:49:24 -0300

    ajuste na saida da lista apresentação

--- listasEncadeadas.py
+++ listasEncadeadas.py
@@ -41,18 +41,16 @@ class ListaEspera:
             if corPrioridade == "V":
                 numeroDeChamada = self.contador_v
                 self.contador_v += 1
-                print(f">>Senha Verde {numeroDeChamada} adicionado à fila.")
+                print(f">>Cartão Verde Senha: {numeroDeChamada} adicionado à fila.")
             elif corPrioridade == "A":
                 numeroDeChamada = self.contador_a
                 self.contador_a += 1
-                print(f">>Senha Amarelo {numeroDeChamada} adicionado à fila.")
+                print(f">>Cartão Amarelo Senha: {numeroDeChamada} adicionado à fila.")
             elif corPrioridade == "S":
-
                 return
             else:
                 print("Cor invalida.")
                 continue
-
             nodo = Nodo(numeroDeChamada, corPrioridade)
             if not self.head:
                 self.head = nodo
@@ -63,15 +61,21 @@ class ListaEspera:
 
     def imprimirListaEspera(self):
         paciente = self.head
+        listaMostrar = []
+        
         if not paciente:
             print("A fila esta vazia.")
             return
         print("\nLista de Espera:")
         while paciente:
             print(
-                f"Cartão {paciente.corPrioridade}{paciente.numeroDeChamada}")
+                f"Cartão: {paciente.corPrioridade} Senha: {paciente.numeroDeChamada}")
+            listaMostrar.append(f'{paciente.corPrioridade}-{paciente.numeroDeChamada}')
+            
             paciente = paciente.seguinte
-
+            
+        print(listaMostrar) 
+           
     def atenderPaciente(self):
         while True:
             if not self.head:
@@ -80,7 +84,7 @@ class ListaEspera:
             atendido = self.head
             self.head = self.head.seguinte
             print(
-                f">>Chamando paciente com Senha {atendido.corPrioridade}{atendido.numeroDeChamada} para atendimento.")
+                f">>Chamando paciente com cartão: {atendido.corPrioridade} Senha: {atendido.numeroDeChamada} para atendimento.")
             acao = input(
                 "Para continuar atendendo digite 'S' ").strip().upper()
             if acao == "S":
@@ -92,21 +96,17 @@ class ListaEspera:
     def menu(self):
         print("\nSistema de inclusão em lista de espera para atendimento médico")
         while True:
-
             opcao = input(
-
                 "\n1 – Adicionar novo paciente a fila\n"
                 "2 – Mostrar todos os pacientes na fila\n"
                 "3 – Chamar paciente da fila\n"
                 "4 – Sair\n"
-                "Escolha uma opção do menu: "
-            )
+                "Escolha uma opção do menu: ")
             match opcao:
                 case "1": self.inserir()
                 case "2": self.imprimirListaEspera()
                 case "3": self.atenderPaciente()
-                case "4": print("Encerrando programa.");break
-                
+                case "4": print("Encerrando programa.");break    
                 case _: print("Opcao invalida. Tente novamente.")
 
 
